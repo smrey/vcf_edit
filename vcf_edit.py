c5d2f3396bf31725fa168ff849f15b1bf2d601c9
Sara Rey <sara.rey@wales.nhs.uk>
2020-03-23 15:25:42 +0000

    don't process indels

--- vcf_edit.py
+++ vcf_edit.py
@@ -28,13 +28,19 @@ def get_args():
             File path to vcf file to be edited. REQUIRED.
             '''))
 
-   # OPTIONAL:
+   # OPTIONAL: TODO Support these options
     optional_options = argument_parser.add_mutually_exclusive_group()
     optional_options.add_argument(
         '-o', '--output_file', action='store_true', default=False,
         help=textwrap.dedent(
             '''
             Add output filename. Default is {input filename}_edited.vcf. OPTIONAL.
+            '''))
+    optional_options.add_argument(
+        '-b', '--bcftools', action='store_true', default=False,
+        help=textwrap.dedent(
+            '''
+           Specify that bcftools variant caller that has been used to generate the VCF. OPTIONAL.
             '''))
 
     return argument_parser.parse_args()
@@ -69,37 +75,56 @@ def multiple_snp_calls(ref_snp, called_snps):
     called_snps = iupac_dict.get(find_iupac)
     if not called_snps:
         # Combination of nucleotides not in dictionary- call uncertain- set to N
-        called_snp = 'N'
+        called_snps = 'N'
     return called_snps
 
 
 def load_vcf(vcf_file_path):
-    vcf_record = []
     try:
         read_vcf = vcf.Reader(open(vcf_file_path, 'r'))
     except TypeError:
         raise FileNotFoundError("VCF file could not be loaded. Check path to VCF has been correctly entered on"
                                 " the command line (flag -v) and that it is a VCF file.")
     # Update vcf metadata to add processing
-    read_vcf.metadata.update({"vcf_edit": ['VCF edited to apply IUPAC uncertainty codes to ALT.']})
+    read_vcf.metadata.update({"vcf_edit": ['VCF edited to apply IUPAC uncertainty codes to ALT snp calls.']})
+    return read_vcf
 
-    # Update ALT base
-    for call in read_vcf:
-        if len(call.ALT) == 1:
-            call.ALT = single_snp_call(call.REF, call.ALT)
-        elif len(call.ALT) > 1:
-            call.ALT = multiple_snp_calls(call.REF, call.ALT)
+
+def parse_vcf(vcf, AF_data_location):
+    vcf_record = []
+    # Update ALT base for SNPs
+    # Do not process indel calls
+    for record in vcf:
+        # Don't process indels
+        if record.POS == 28988: # TEMP
+            break # TEMP
+        if record.is_indel:
+            continue
+        # Multisample vcfs not supported- TODO if needed
+        if len(record.samples) != 1:
+            raise NotImplementedError(f"Multisample VCFs not supported. Only one sample per record supported.")
+        print(record.samples[0].data[AF_data_location])
+        # Process snps
+        if len(record.ALT) == 1:
+            record.ALT = single_snp_call(record.REF, record.ALT)
+        elif len(record.ALT) > 1:
+            record.ALT = multiple_snp_calls(record.REF, record.ALT)
         else:
-            raise Exception(f"No alt call at position. Cannot parse VCF {vcf_file_path}")
-        vcf_record.append(call)
-    write_vcf(vcf_record, read_vcf)
+            raise Exception(f"No alt call at position {record.POS}. Cannot parse VCF") # TODO check this is actually doing anything
+        vcf_record.append(record)
+    return vcf_record
+
 
 
 def main():
     __version__ = '0.0.1'
-    __updated__ = '20/03/2020'
+    __updated__ = '23/03/2020'
     args = get_args()
-    load_vcf(args.path_to_vcf)
+    vcf_data = load_vcf(args.path_to_vcf)
+    # from bcftools the location of the allele depth (per allele)- TODO link to option for variant caller
+    index_of_AD = 2
+    updated_vcf_data = parse_vcf(vcf_data, index_of_AD)
+    write_vcf(updated_vcf_data, vcf_data)
 
 
 if __name__ == '__main__':
