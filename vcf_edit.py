e9fb1262850117ac07e14b8ae0503c04d217b0b8
Sara Rey <sara.rey@wales.nhs.uk>
2020-03-25 18:46:28 +0000

    altered allele depths

--- vcf_edit.py
+++ vcf_edit.py
@@ -76,16 +76,42 @@ def write_vcf(records_to_write, read_vcf, output_vcf="output.vcf"):
         vcf_writer.write_record(record)
 
 
-def apply_iupac(bases):
+def apply_iupac(bases_dict):
     # Order in alphabetical order for lookup
-    bases.sort()
-    bases = tuple(bases)
+    base_list = []
+    [base_list.append(b) for b in bases_dict.keys()]
+    base_list.sort()
+    bases = tuple(base_list)
     find_iupac = bases
     called = iupac_dict.get(find_iupac)
     if not called:
         # Combination of nucleotides not in dictionary- call uncertain- set to N
         called = iupac_dict.get('uncertainty')
     return called
+
+
+def format_data_format_creator(format_dict, base_pos, iupac_uncertainty=False):
+    format_titles = []
+    format_values = []
+    for format_title, format_value in format_dict.items():
+        # Only ever one entry for GT, not one entry per allele as for others
+        format_titles.append(format_title)
+        try:
+            format_values.append(format_value[base_pos])
+        except (IndexError, TypeError):
+            # Single value only
+            format_values.append(format_value)
+    ca = namedtuple('CallData', format_titles)
+    # Case where only one alt base comes through
+    if not iupac_uncertainty:
+        caller_data = ca(format_values[0], format_values[1],
+                         format_values[2])  # TODO Fix this line to be flexible to len(format_titles)
+
+    # Case where multiple alt bases come through as iupac uncertainty code
+    else:
+        caller_data = ca(format_values[0], format_values[1],)  # TODO Fix this line to be flexible to len(format_titles)
+
+    return caller_data
 
 
 def reads_to_proportions(total_depth, depths_per_base):
@@ -152,7 +178,6 @@ def parse_vcf(vcf):
         # Do not process indels further
         if is_indel:
             # Do not propagate indels called through this workflow
-            #vcf_record.append(record)
             continue
 
         # Process all sites that are single nucleotides
@@ -182,7 +207,7 @@ def parse_vcf(vcf):
         all_bases = [str(base) for base in record.ALT if base is not None]
         all_bases.insert(0, record.REF)
 
-        # Obtain correct FORMAT data for site
+        # Obtain correct FORMAT data for site (correct present fields associated with correct entry)
         format_field_dictionary = parse_out_sample_format(record.FORMAT, record.samples[0].data)
         try:
             supporting_reads = format_field_dictionary['AD']
@@ -199,55 +224,57 @@ def parse_vcf(vcf):
         supporting_proportions = reads_to_proportions(total_bases_at_site, supporting_reads)
 
         # Filtering on proportion of supporting bases
-        # TODO remove depth annotations for alleles that are no longer coming through- URGENT
-        bases_to_process = []
+        bases_to_process = {}
         iupac_uncertainty = False
+        # Get AD from INFO field, once per site
+        site_info_allele_depth = record.INFO.get('AD')
         for i, b in enumerate(all_bases):
+            # Obtain allele depth (supporting reads) for this base
+            info_allele_depth = site_info_allele_depth[i]
+            format_allele_depth = supporting_reads[i]
+            # Sanity check- both allele depths same
+            if info_allele_depth != format_allele_depth:
+                raise ValueError(
+                    f"Total number of high quality bases could not be unambiguously determined. Proportion"
+                    f" of bases supporting each read cannot be determined.")
+
             # Pass through only bases representing 0.75 or more of the total reads
             if supporting_proportions[i] > confident_allele_support_proportion:
                 # Only one base can meet the above criteria
-                print(record.POS)
-                print(supporting_reads)
-                print(supporting_reads[i])
-                print(record.samples[0].data)
-                print(record.samples)
-                print(record.samples[0].site)
-                caller_data = record.samples[0].data
-                #caller_data = make_calldata_tuple(('GT', 'PL')) # requires input in the 'fields' format, which is the samp_fmt.split(':')
-                print(caller_data)
-                print(record.INFO)
-                print(f"fields are {caller_data._fields}")
-                caller_data = make_calldata_tuple(('GT', 'PL', 'AD'))
-                ca = namedtuple('CallData', ['GT', 'PL', 'AD'])
-                caller_data = ca(3, 1, 677)
-                print(caller_data)
-                record.samples[0].data = caller_data
-                print(record.samples)
-                c = Caller(record.samples[0].site, record.samples[0].sample, caller_data)
-                print(c.data)
-                print(c.data.GT)
-                print(c._format_cache)
-                c.data.GT = 3 #c.data.GT_replace(GT=3)
-                print(c.data)
-                #record.samples[0].data = {'GT':0, 'PL':3, 'AD':45}
-                print(record.samples[0].data)
                 # Edit INFO and FORMAT fields to remove reads where bases have been removed
+                # Update INFO entries
+                record.INFO['AD'] = format_allele_depth
+
+                # Update FORMAT entries
+                caller_data = format_data_format_creator(format_field_dictionary, i)
+                record.samples[0].data = caller_data
 
                 # If final call is the same as the reference base alt should be set to .
                 if b == record.REF:
                     record.ALT = '.'
                 else:
                     record.ALT = b
+
             # Sites with bases with support from >0.25 but <0.75 of the total reads are uncertain
             elif minimum_allele_support_proportion < supporting_proportions[i] < confident_allele_support_proportion:
-                # Edit INFO and FORMAT fields to supply evidence for ambiguity base (summation of reads supporting)
-
-                bases_to_process.append(b)
+                bases_to_process[b] = supporting_reads[i]
                 iupac_uncertainty = True
 
         # Process uncertainty sites
         if iupac_uncertainty:
             record.ALT = apply_iupac(bases_to_process)
+            # Calculate depth of reads supporting any of the bases included in the iupac code- update
+            updated_format_allele_depth = (sum(bases_to_process.values()))
+
+            # Edit INFO and FORMAT fields to supply evidence for ambiguity base (summation of reads supporting)
+            record.INFO['AD'] = updated_format_allele_depth
+
+            # Update FORMAT entries- retain unknown genotype and allele depth information only- single allele only (0)
+            print(record.FORMAT)
+            record.FORMAT = 'GT:AD'
+            new_format_field_dictionary = {'GT': './.', 'AD': updated_format_allele_depth}
+            caller_data = format_data_format_creator(new_format_field_dictionary, 0, iupac_uncertainty=True)
+            record.samples[0].data = caller_data
 
         # Update vcf records
         vcf_record.append(record)
